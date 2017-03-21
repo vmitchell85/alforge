ef512bf0fab09178d194daa46c7868db5888559e
Vince Mitchell <vincent.mitchell@gmail.com>
2017-03-21 08:32:38 -0400

    inital

---
+++ src/Forge.php
@@ -0,0 +1,135 @@
+
+<?php
+
+use Alfred\Workflows\Workflow;
+
+class Forge
+{
+    protected $data;
+    protected $token;
+    
+    protected $cacheDir;
+    protected $dataCache;
+    protected $authCache;
+
+    public function __construct()
+    {
+        $this->cacheDir = getenv('alfred_workflow_cache');
+        $this->dataCache = getenv('alfred_workflow_cache') . '/data.json';
+        $this->authCache = getenv('alfred_workflow_cache') . '/auth.txt';
+        
+        if( !file_exists($this->cacheDir) ){
+            mkdir($this->cacheDir);
+        }
+        
+        $this->token = file_exists($this->authCache) ? file_get_contents($this->authCache): '';
+        $this->data = file_exists($this->dataCache) ? json_decode(file_get_contents($this->dataCache)) : $this->loadCache();
+    }
+
+    public function loadCache($reload = False)
+    {
+        $this->data = [];
+        $this->data['servers'] = [];
+        
+        $response = $this->fetchData('https://forge.laravel.com/api/v1/servers', 'GET');
+
+        $serverCount = 0;
+        
+        if( $response->servers ){
+            foreach( $response->servers as $server ){
+                $serverCount++;
+                $sitesResponse = $this->fetchData('https://forge.laravel.com/api/v1/servers/'.$server->id.'/sites/', 'GET');
+                $server->sites = $sitesResponse->sites;
+                array_push($this->data['servers'], $server);
+            }
+
+            file_put_contents($this->dataCache, json_encode($this->data));
+        }
+
+        if( $reload ){
+            echo "Loaded $serverCount servers!";
+        }
+    }
+
+    public function fetchData($url, $method = 'POST', $data = '')
+    {
+        $authorization = "Authorization: Bearer " . $this->token;
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
+    }
+
+    public function setKey($key)
+    {
+
+        file_put_contents($this->authCache, $key);
+
+        echo 'API Key Has Been Set';
+    }
+
+    public function clearData()
+    {
+        unlink($this->authCache);
+        unlink($this->dataCache);
+
+        echo 'All Data Deleted!';
+    }
+
+    public function execute($command)
+    {
+        $cmdParts = split(' ', $command);
+
+        switch ($cmdParts[0]) {
+            case 'open':
+                if( $cmdParts[2] ){
+                    echo "https://forge.laravel.com/servers/$cmdParts[1]/sites/$cmdParts[2]";
+                }
+                else{
+                    echo "https://forge.laravel.com/servers/$cmdParts[1]";
+                }
+
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+
+    public function search($query)
+    {
+        $workflow = new Workflow;
+
+        foreach ($this->data->servers as $server) {
+            if( strpos($server->name, $query) > -1 ){
+                $workflow->result()
+                    ->uid($server->id)
+                    ->title('Server: ' . $server->name)
+                    ->subtitle($server->region)
+                    ->arg('open ' . $server->id)
+                    ->valid(true);
+            }
+            
+            foreach ($server->sites as $site) {
+                if( strpos($site->name, $query) > -1 ){
+                    $workflow->result()
+                        ->uid($site->id)
+                        ->title('Site: ' . $site->name)
+                        ->subtitle($server->name)
+                        ->arg('open ' . $server->id . ' ' . $site->id)
+                        ->valid(true);
+                }
+            }
+        }
+
+
+        echo $workflow->output();
+    }
+}
