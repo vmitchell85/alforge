22ff6369bc70052ea76ef3e6123f6fd905ccfa4c
Vince Mitchell <vincent.mitchell@gmail.com>
2017-03-21 22:51:09 -0400

    extract open

---
+++ src/Open.php
@@ -0,0 +1,24 @@
+<?php
+
+include('Forge.php');
+use Alfred\Workflows\Workflow;
+
+class Open extends Forge
+{
+    public function search($query)
+    {
+        echo $this->allSearch($query);
+    }
+
+    public function execute($command)
+    {
+        $cmdParts = split(' ', $command);
+
+        if( $cmdParts[1] ){
+            $this->respond("https://forge.laravel.com/servers/$cmdParts[0]/sites/$cmdParts[1]");
+        }
+        else{
+            $this->respond("https://forge.laravel.com/servers/$cmdParts[0]");
+        }
+    }
+}
