ea746169aaffec272070c615090c6356a28e9d8a
Vince Mitchell <vincent.mitchell@gmail.com>
2017-03-21 22:51:02 -0400

    enable server rebooting

---
+++ src/Reboot.php
@@ -0,0 +1,23 @@
+<?php
+
+include('Forge.php');
+use Alfred\Workflows\Workflow;
+
+class Reboot extends Forge
+{
+    public function search($query)
+    {
+        echo $this->serverSearch($query);
+    }
+
+    public function execute($server_id)
+    {
+        $server = $this->getServerInfo($server_id);
+
+        if($this->confirm("Are you sure you want to reboot the server `$server->name`?")){
+            $response = $this->apiRequest("https://forge.laravel.com/api/v1/servers/$server->id/reboot");
+            $this->respond("`$server->name` is rebooting...");
+        }
+
+    }
+}
