7a131e6842ada14929a942667378d6ad57d228f3
Vince Mitchell <vincent.mitchell@gmail.com>
2017-04-06 12:53:47 -0400

    add copy env to clipboard

---
+++ src/alforge/Env.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AlForge;
+
+use AlForge\Forge;
+use Alfred\Workflows\Workflow;
+
+class Env extends Forge
+{
+    public function search($query)
+    {
+        echo $this->siteSearch($query);
+    }
+
+    public function execute($command)
+    {
+
+        $cmdParts = split(' ', $command);
+
+        $server = $this->getServerInfo($cmdParts[0]);
+        $site = $this->getSiteInfo($cmdParts[0], $cmdParts[1]);
+
+        $response = $this->apiRequest("https://forge.laravel.com/api/v1/servers/$cmdParts[0]/sites/$cmdParts[1]/env", "GET");
+
+        $this->respond($response);
+
+    }
+}
